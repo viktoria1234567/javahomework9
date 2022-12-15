7231ba662d87727f4d0295fd5a6c2fcb2fd84dfa
Viktoria <viktoriapredko1@gmail.com>
2022-12-15 14:59:39 +0300

    Solve task

---
+++ program/Demo.java
@@ -0,0 +1,10 @@
+package program;
+
+import program.login.Login;
+
+public class Demo {
+    public static void main(String[] args) {
+        new Login();
+
+    }
+}
---
+++ program/exception/WrongPasswordException.java
@@ -0,0 +1,19 @@
+package program.exception;
+
+public class WrongPasswordException extends Exception {
+    private String detail;
+
+    public WrongPasswordException() {
+        detail = "Incorrect password";
+    }
+
+    public WrongPasswordException(String message) {
+        super(message);
+        detail = message;
+    }
+
+    @Override
+    public String toString() {
+        return "WrongPasswordException: " + detail;
+    }
+}
